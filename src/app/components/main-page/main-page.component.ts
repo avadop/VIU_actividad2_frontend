4c700c9621b9ea855212cc9683379098a4fb38c7
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2023-07-10 16:48:58 +0200

    Continuacion pantalla inicio

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+const infoTextLoggedOut:string = "Para ello es necesario que te registres o, si ya estás registrado, inicies sesión.";
+const infoTextLoggedIn:string = "¡Ya estás listo para comenzar!";
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+  public infoText: String;
 
+  public userLogged: Boolean;
+
+
+  constructor() {
+    this.userLogged = false;
+    this.infoText = this.userLogged ? infoTextLoggedIn : infoTextLoggedOut;
+  }
 }
