4564f14c1284c1aefa7f1ea13470975ed758c83d
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2023-07-11 14:39:34 +0200

    pantallas creadas para redirecciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    RegistroUsuarioComponent,
+    InicioSesionComponent
   ],
   imports: [
     BrowserModule,
